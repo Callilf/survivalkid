50eea11a6beaf2865f938ff287baf3781b2b9e72
baaleze <baaleze@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-04 21:55:15 +0000

    Ajout de la méthode move() qui vbouge l'objet suivant la vitesse.

--- survivalkid/SurvivalKid/src/com/survivalkid/game/util/MoveUtil.java
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/util/MoveUtil.java
@@ -2,5 +2,10 @@ package com.survivalkid.game.util;
 
 public final class MoveUtil {
 
-	private MoveUtil() {};
+	public static final int GRAVITY = 5;
+	public static final int MAX_X = 500;
+	public static final int MAX_Y = 300;
+
+	private MoveUtil() {
+	};
 }
