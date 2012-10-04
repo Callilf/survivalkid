73ef2d5632b52f7b1ff2b0f97a266b6cba7f8f11
thom159123@gmail.com <thom159123@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-04 20:10:21 +0000

    Ajout de EnemyManager + attributs

--- survivalkid/SurvivalKid/src/com/survivalkid/game/manager/EnemyManager.java
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/manager/EnemyManager.java
@@ -2,11 +2,13 @@ package com.survivalkid.game.manager;
 
 import java.util.List;
 
+import com.survivalkid.game.entity.enemy.EnemyEntity;
 import com.survivalkid.game.entity.item.ItemEntity;
 
-public class ItemManager extends ObjectManager {
+public class EnemyManager extends ObjectManager {
 
-	private List<ItemEntity> itemList;
+	private List<EnemyEntity> enemyList;
+	private long lastEnemy;
 	
 	public void create() {
 		// TODO Auto-generated method stub
