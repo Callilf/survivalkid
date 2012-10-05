5935c5b0aa0a582a71e3defabdaf543b25520c55
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-05 22:35:42 +0000

    GameEntity a un AnimatedSprite au lieu d'un Rect !

--- survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/EnemyEntity.java
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/EnemyEntity.java
@@ -1,7 +1,6 @@
 package com.survivalkid.game.entity.enemy;
 
-import android.graphics.Rect;
-
+import com.survivalkid.game.core.AnimatedSprite;
 import com.survivalkid.game.entity.GameEntity;
 
 public abstract class EnemyEntity extends GameEntity {
@@ -9,7 +8,7 @@ public abstract class EnemyEntity extends GameEntity {
 	protected int dammage;
 	protected int difficulty;
 	
-	public EnemyEntity(String _name, Rect _sprite, int _dammage, int _difficulty) {
+	public EnemyEntity(String _name, AnimatedSprite _sprite, int _dammage, int _difficulty) {
 		super(_name, _sprite);
 		dammage = _dammage;
 		difficulty = _difficulty;
--- survivalkid/SurvivalKid/src/com/survivalkid/game/entity/item/ItemEntity.java
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/entity/item/ItemEntity.java
@@ -1,14 +1,13 @@
 package com.survivalkid.game.entity.item;
 
-import android.graphics.Rect;
-
+import com.survivalkid.game.core.AnimatedSprite;
 import com.survivalkid.game.entity.GameEntity;
 
 public abstract class ItemEntity extends GameEntity {
 
 	protected int power;
 	
-	public ItemEntity(String _name, Rect _sprite, int _power) {
+	public ItemEntity(String _name, AnimatedSprite _sprite, int _power) {
 		super(_name, _sprite);
 		power = _power;
 	}
