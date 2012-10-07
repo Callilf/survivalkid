ddfa9c98dc0620a705aa94885a28d4cdfc7b29db
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-07 20:59:24 +0000

    définition des animations des persos dan le constructeur de Personage

--- trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/core/Constants.java
+++ trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/core/Constants.java
@@ -12,5 +12,11 @@ public final class Constants {
 	public class PersonageConstants {
 		public static final int PERSO_YUGO = 0;
 		public static final int PERSO_YUNA = 1;
+		
+		public static final String ANIM_STAND = "stand";
+		public static final String ANIM_RUN = "run";
+		public static final String ANIM_JUMPUP = "jumpup";
+		public static final String ANIM_JUMPDOWN = "jumpdown";
+
 	}
 }
