c08f420e0011d4f67ae58e71155509bc162d77fd
thom159123@gmail.com <thom159123@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-09 21:25:15 +0000

    Ajustement gravité + vitesse saut

--- survivalkid/SurvivalKid/src/com/survivalkid/game/entity/personage/Personage.java
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/entity/personage/Personage.java
@@ -34,7 +34,7 @@ public class Personage extends GameEntity {
 	public Personage(int perso, Bitmap bitmap, int x, int y, int nbColum,
 			int nbRows) {
 		super("CharacterA", bitmap, x, y, nbColum, nbRows);
-		gravity = 12;
+		gravity = 4;
 		persoType = perso;
 		movePersoManager = new MovePersoManager();
 
