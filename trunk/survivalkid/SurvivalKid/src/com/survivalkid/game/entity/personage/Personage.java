22e1b9951042a2fd8b7b5d05c43a322e667de1f1
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-11 19:40:53 +0000

    déplacement du sprite de yuna pour avoir une hitbox qui match toutes les animations

--- trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/entity/personage/Personage.java
+++ trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/entity/personage/Personage.java
@@ -53,7 +53,7 @@ public class Personage extends GameEntity {
 			addAnimation(PersonageConstants.ANIM_RUN, new int[]{1,2,4,6,7,8,9,10,11,12,13,14}, 20);
 			addAnimation(PersonageConstants.ANIM_JUMPUP, new int[]{39,40,41}, 13);
 			addAnimation(PersonageConstants.ANIM_JUMPDOWN, new int[]{17,18,19}, 13);
-			redefineHitBox( (sprite.getWidth()*35) / 100, (sprite.getHeight()*10) / 100, (sprite.getWidth()*40) / 100, (sprite.getHeight()*84) / 100);
+			redefineHitBox( (sprite.getWidth()*50) / 100, (sprite.getHeight()*10) / 100, (sprite.getWidth()*40) / 100, (sprite.getHeight()*84) / 100);
 			break;
 		}
 		
@@ -91,7 +91,7 @@ public class Personage extends GameEntity {
 			play(PersonageConstants.ANIM_JUMPUP, false, true);
 		} else if (isJumpingDown) {
 			play(PersonageConstants.ANIM_JUMPDOWN, false, true);
-		} else if (isMovingHorizontally || movePersoManager.isLeftEnabled || movePersoManager.isRightEnabled) {
+		} else if (movePersoManager.isLeftEnabled || movePersoManager.isRightEnabled) {
 			play(PersonageConstants.ANIM_RUN, true, true);
 		} else {
 			play(PersonageConstants.ANIM_STAND, false, true);
