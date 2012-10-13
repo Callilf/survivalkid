291934b6242bf2a28f055955f10d51ecc2ebe059
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-13 14:15:38 +0000

    Ajout de l'ennemi "Caterpillar" (meme si c'est pas une chenille mais je savais pas dire limace et j'ai eu la flemme de chercher sur internet) + cleaning des sprites des persos

---
+++ trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/impl/Caterpillar.java
@@ -0,0 +1,51 @@
+package com.survivalkid.game.entity.enemy.impl;
+
+import android.graphics.Bitmap;
+
+import com.survivalkid.game.core.Constants.DirectionConstants;
+import com.survivalkid.game.entity.GameEntity;
+import com.survivalkid.game.entity.enemy.EnemyEntity;
+
+public class Caterpillar extends EnemyEntity {
+
+	/**
+	 * Create enemy
+	 */
+	public Caterpillar(Bitmap _bitmap, int _x, int _y, int _nbColums, int _nbRows) {
+		super("Caterpillar", _bitmap, _x, _y, _nbColums, _nbRows, 10, 3);
+		
+		redefineHitBox( (sprite.getWidth()*18) / 100, (sprite.getHeight()*65) / 100, (sprite.getWidth()*74) / 100, (sprite.getHeight()*35) / 100);
+		addAnimation("crawl", new int[]{0,1,2,3,2,1}, 13);
+		play("crawl", true, true);
+	}
+	
+	@Override
+	public void update(long gameTime) {
+		super.update(gameTime);
+		
+		if(touchLeft()) {
+			direction = DirectionConstants.RIGHT;
+		} else if(touchRight()) {
+			direction = DirectionConstants.LEFT;
+		}
+		
+		if(direction == DirectionConstants.LEFT) {
+			sprite.offset(-2, 0);
+		} else {
+			sprite.offset(2, 0);
+		}
+	}
+
+	@Override
+	public void collide(GameEntity _gameEntity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
