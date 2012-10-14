90e57876dd40d17cda82e2fee10a174280abd155
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-14 15:47:51 +0000

    bitmapUtil

--- survivalkid/SurvivalKid/src/com/survivalkid/game/entity/personage/Personage.java
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/entity/personage/Personage.java
@@ -1,7 +1,6 @@
 package com.survivalkid.game.entity.personage;
 
 import android.content.res.Resources;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.Log;
---
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/util/BitmapUtil.java
@@ -0,0 +1,42 @@
+package com.survivalkid.game.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public final class BitmapUtil {
+	private final static int IMAGE_MAX_SIZE = 200;
+
+	private BitmapUtil(){}
+	
+	public static Bitmap decodeFile(File f){
+	    Bitmap b = null;
+	    try {
+	        //Decode image size
+	        BitmapFactory.Options o = new BitmapFactory.Options();
+	        o.inJustDecodeBounds = true;
+
+	        FileInputStream fis = new FileInputStream(f);
+	        BitmapFactory.decodeStream(fis, null, o);
+	        fis.close();
+
+	        int scale = 1;
+	        if (o.outHeight > IMAGE_MAX_SIZE || o.outWidth > IMAGE_MAX_SIZE) {
+	            scale = (int)Math.pow(2, (int) Math.round(Math.log(IMAGE_MAX_SIZE / 
+	               (double) Math.max(o.outHeight, o.outWidth)) / Math.log(0.5)));
+	        }
+
+	        //Decode with inSampleSize
+	        BitmapFactory.Options o2 = new BitmapFactory.Options();
+	        o2.inSampleSize = scale;
+	        fis = new FileInputStream(f);
+	        b = BitmapFactory.decodeStream(fis, null, o2);
+	        fis.close();
+	    } catch (IOException e) {
+	    }
+	    return b;
+	}
+}
