1ebeb8356f82119aed69d49b8aa1dc7522fdd8b6
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-17 17:37:00 +0000

    la génération d'ennemis génère aussi des limaces now !

--- trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/impl/Caterpillar.java
+++ trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/impl/Caterpillar.java
@@ -1,12 +1,16 @@
 package com.survivalkid.game.entity.enemy.impl;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 
+import com.survivalkid.R;
 import com.survivalkid.game.core.Constants.DirectionConstants;
 import com.survivalkid.game.entity.GameEntity;
 import com.survivalkid.game.entity.enemy.EnemyEntity;
 import com.survivalkid.game.entity.personage.Personage;
+import com.survivalkid.game.util.BitmapUtil;
+import com.survivalkid.game.util.MoveUtil;
 
 public class Caterpillar extends EnemyEntity {
 
@@ -17,6 +21,8 @@ public class Caterpillar extends EnemyEntity {
 			int _nbRows) {
 		super("Caterpillar", _bitmap, _x, _y, _nbColums, _nbRows, 10, 3);
 
+		affectedByWalls = false;
+		
 		redefineHitBox((sprite.getWidth() * 18) / 100,
 				(sprite.getHeight() * 65) / 100,
 				(sprite.getWidth() * 74) / 100, (sprite.getHeight() * 35) / 100);
@@ -28,16 +34,14 @@ public class Caterpillar extends EnemyEntity {
 	public void update(long gameTime) {
 		super.update(gameTime);
 
-		if (touchLeft()) {
-			direction = DirectionConstants.RIGHT;
-		} else if (touchRight()) {
-			direction = DirectionConstants.LEFT;
-		}
-
 		if (direction == DirectionConstants.LEFT) {
 			sprite.offset(-2, 0);
 		} else {
 			sprite.offset(2, 0);
+		}
+		
+		if(sprite.getX() < -20 || sprite.getX() > MoveUtil.SCREEN_WIDTH + sprite.getWidth()) {
+			dead = true;
 		}
 	}
 
@@ -57,8 +61,18 @@ public class Caterpillar extends EnemyEntity {
 
 	public static EnemyEntity generateRandowStartingPosition(
 			Point playerPosition) {
-		// TODO Auto-generated method stub
-		return null;
+		int random =  (int) (Math.random() * 100);
+		Caterpillar cat = null;
+		if(random<49) {
+			cat = new Caterpillar(BitmapUtil.createBitmap(R.drawable.caterpillar),
+					-20, 380, 4, 1);
+			cat.direction = DirectionConstants.RIGHT;
+		} else {
+			cat = new Caterpillar(BitmapUtil.createBitmap(R.drawable.caterpillar),
+					MoveUtil.SCREEN_WIDTH + 20, 380, 4, 1);
+			cat.direction = DirectionConstants.LEFT;
+		}
+		return cat;
 	}
 
 }
