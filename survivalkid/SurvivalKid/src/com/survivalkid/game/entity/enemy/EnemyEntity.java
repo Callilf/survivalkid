cd8656f5da0e428ade15865c8d0180b4b27cf521
thom159123@gmail.com <thom159123@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-21 13:18:45 +0000

    Ajout de la météorite. Elle n'est pas complètement terminée.

--- survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/EnemyEntity.java
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/EnemyEntity.java
@@ -11,6 +11,19 @@ public abstract class EnemyEntity extends GameEntity {
 	protected int dammage;
 	protected int difficulty;
 
+	/**
+	 * 
+	 * Constructor called by parents class
+	 * 
+	 * @param _name name of the entity
+	 * @param bitmap image of the entity
+	 * @param x position x initial
+	 * @param y position y initial
+	 * @param nbColum number of column in the sprite
+	 * @param nbRows number of line in the sprite
+	 * @param _dammage how much the perso loose life when he hit the enemy
+	 * @param _difficulty the index of difficulty of the enemy
+	 */
 	public EnemyEntity(String _name, Bitmap _sprite, int _x, int _y,
 			int _nbColumns, int _nbRows, int _dammage, int _difficulty) {
 		super(_name, _sprite, _x, _y, _nbColumns, _nbRows);
