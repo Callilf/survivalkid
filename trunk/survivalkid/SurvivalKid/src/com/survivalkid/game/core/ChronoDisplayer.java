db3d3a0461dd5ba96653d63f0a644b583dd2fcab
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2012-10-27 11:01:46 +0000

    Affichage du temps en haut a gauche

---
+++ trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/core/ChronoDisplayer.java
@@ -0,0 +1,39 @@
+package com.survivalkid.game.core;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Typeface;
+
+public class ChronoDisplayer {
+
+	private Paint paint;
+	private String stringToDisplay;
+	private Point position;
+	
+	public ChronoDisplayer(int _x, int _y) {
+		//Text
+		paint = new Paint(); 
+		paint.setTextSize(20);
+		paint.setTypeface(Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD));
+		paint.setColor(Color.WHITE);
+
+		position = new Point(_x,_y);
+	}
+
+	public void setTime(long timeInMillis) {
+		Date date = new Date(timeInMillis);
+		DateFormat formatter = new SimpleDateFormat("mm:ss,SSS");
+		stringToDisplay = formatter.format(date);
+	}
+	
+	public void draw(Canvas canvas) {
+		canvas.drawText( stringToDisplay, position.x, position.y, paint);
+	}
+
+}
