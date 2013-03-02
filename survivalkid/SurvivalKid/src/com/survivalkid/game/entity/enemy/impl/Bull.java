3a04f5f117a9a8d85dc7f0cc3288c9611253a631
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2013-03-02 17:38:46 +0000

    warning before the bull thrust

--- survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/impl/Bull.java
+++ survivalkid/SurvivalKid/src/com/survivalkid/game/entity/enemy/impl/Bull.java
@@ -1,8 +1,10 @@
 package com.survivalkid.game.entity.enemy.impl;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Point;
 
+import com.survivalkid.R;
 import com.survivalkid.game.core.AnimatedSprite;
 import com.survivalkid.game.core.Constants.DirectionConstants;
 import com.survivalkid.game.core.enums.SpriteEnum;
@@ -10,6 +12,8 @@ import com.survivalkid.game.core.enums.StateEnum;
 import com.survivalkid.game.entity.Life.EnumLife;
 import com.survivalkid.game.entity.enemy.EnemyEntity;
 import com.survivalkid.game.entity.personage.Personage;
+import com.survivalkid.game.singleton.GameContext;
+import com.survivalkid.game.util.BitmapUtil;
 import com.survivalkid.game.util.MoveUtil;
 
 public class Bull extends EnemyEntity {
@@ -17,7 +21,14 @@ public class Bull extends EnemyEntity {
 	private static final int VITESSE_BULL = 15;
 	private static final int SPEED_COLLISION_X = 20;
 	private static final int SPEED_COLLISION_Y = -30;
+	
+	private static final int WARNING_DURATION = 2000;
 
+	private Bitmap warning;
+	private boolean inWarning;
+	private long warningExpiration;
+	private int warningX;
+	private int warningY;
 	private AnimatedSprite deathAnim;
 	
 	/** Default constructor. */
@@ -31,6 +42,18 @@ public class Bull extends EnemyEntity {
 	private void init() {
 		gravity = 2;
 		affectedByWalls = false;
+		
+		warning = BitmapUtil.createBitmap(R.drawable.bull_warning);
+		inWarning = true;
+		active = false;
+		warningExpiration = GameContext.getSingleton().gameDuration + WARNING_DURATION;
+		if(direction == DirectionConstants.LEFT) {
+			warningX = MoveUtil.SCREEN_WIDTH - warning.getWidth();
+		} else {
+			warningX = 0;
+		}
+		warningY = (int) (MoveUtil.GROUND * 0.75f);
+
 
 		redefineHitBox((sprite.getWidth() * 25) / 100, (sprite.getHeight() * 20) / 100, (sprite.getWidth() * 70) / 100,
 				(sprite.getHeight() * 80) / 100);
@@ -51,6 +74,12 @@ public class Bull extends EnemyEntity {
 
 		if (sprite.getX() + sprite.getWidth() < 0 || sprite.getX() > MoveUtil.SCREEN_WIDTH) {
 			dead = true;
+		}
+		
+		if(inWarning && warningExpiration < gameDuration) {
+			inWarning = false;
+			active = true;
+			initSpeed();
 		}
 		
 		super.update(gameDuration);
@@ -81,7 +110,11 @@ public class Bull extends EnemyEntity {
 		if (dying) {
 			deathAnim.draw(canvas, DirectionConstants.RIGHT);
 		} else {
-			super.draw(canvas);
+			if(inWarning) {
+				canvas.drawBitmap(warning, warningX, warningY, null);
+			} else {
+				super.draw(canvas);
+			}
 		}
 	}
 
@@ -102,8 +135,11 @@ public class Bull extends EnemyEntity {
 			sprite.setY(380);
 			direction = DirectionConstants.LEFT;
 		}
-			
 		
+		init();
+	}
+	
+	private void initSpeed() {
 		// init speed
 		setSpeedY(0);
 		if (direction == DirectionConstants.LEFT) {
@@ -111,8 +147,6 @@ public class Bull extends EnemyEntity {
 		} else {
 			setSpeedX(VITESSE_BULL);
 		}
-
-		init();
 	}
 
 }
