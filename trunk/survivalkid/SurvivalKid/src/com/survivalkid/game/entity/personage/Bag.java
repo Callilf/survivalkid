4836a4b646ebcb40f5d8ae1615643e9c9513917c
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2013-03-13 07:19:28 +0000

    few improvements on the bag

--- trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/entity/personage/Bag.java
+++ trunk/survivalkid/SurvivalKid/src/com/survivalkid/game/entity/personage/Bag.java
@@ -1,6 +1,8 @@
 package com.survivalkid.game.entity.personage;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.view.MotionEvent;
 
@@ -9,29 +11,38 @@ import com.survivalkid.game.core.Constants.DirectionConstants;
 import com.survivalkid.game.core.enums.SpriteEnum;
 import com.survivalkid.game.entity.item.ItemEntity;
 import com.survivalkid.game.singleton.GameContext;
+import com.survivalkid.game.util.CollisionUtil;
 import com.survivalkid.game.util.MoveUtil;
 
 public class Bag {
 
-	private static final int FREEZE_TIME_DURATION = 100;
+	private static final int FREEZE_TIME_DURATION = 50;
 	private ItemEntity slot;
 	private boolean locked;
 	private AnimatedSprite sprite;
 	private Rect touchBox;
-	
+
 	private long freezeExctinction;
+
+	/** Balloon hitbox for debug. */
+	public Paint touchBoxPainter;
 
 	public Bag() {
 		sprite = new AnimatedSprite(SpriteEnum.BAG_SLOT);
 		sprite.setX(MoveUtil.myBag.x);
 		sprite.setY(MoveUtil.myBag.y);
 		sprite.play("unlocked", false, true);
-		
+
+		touchBoxPainter = new Paint();
+		touchBoxPainter.setColor(Color.MAGENTA);
+		touchBoxPainter.setAlpha(200);
+
 		initTouchBox();
 	}
-	
+
 	public void initTouchBox() {
-		touchBox = new Rect(sprite.getX(), sprite.getY(), sprite.getX()+sprite.getWidth(), sprite.getY()+sprite.getHeight());
+		touchBox = new Rect(sprite.getX() - sprite.getWidth() / 3, sprite.getY() - sprite.getHeight()/3, sprite.getX() + sprite.getWidth()
+				+ sprite.getWidth() / 3, sprite.getY() + sprite.getHeight());
 	}
 
 	public void addItem(ItemEntity item) {
@@ -39,23 +50,28 @@ public class Bag {
 			slot = item;
 		}
 	}
-	
+
 	public void checkTouched(MotionEvent event) {
-		if(freezeExctinction < GameContext.getSingleton().getCurrentTimeMillis() && touchBox.contains((int)event.getX(), (int)event.getY())) {
-			toggleLocked();
-			freezeExctinction = GameContext.getSingleton().getCurrentTimeMillis() + FREEZE_TIME_DURATION;
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			if (freezeExctinction < GameContext.getSingleton().getCurrentTimeMillis()
+					&& touchBox.contains((int) event.getX(), (int) event.getY())) {
+				toggleLocked();
+				freezeExctinction = GameContext.getSingleton().getCurrentTimeMillis() + FREEZE_TIME_DURATION;
+			}
 		}
 	}
 
 	public void update(long gameDuration) {
 		sprite.update(gameDuration, DirectionConstants.RIGHT);
 	}
-	
+
 	public void draw(Canvas canvas) {
 		sprite.draw(canvas, DirectionConstants.RIGHT);
+		if (CollisionUtil.displayHitBoxes) {
+			canvas.drawRect(touchBox, touchBoxPainter);
+		}
 	}
-	
-	
+
 	/**
 	 * @return the locked
 	 */
