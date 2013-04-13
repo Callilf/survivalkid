d90a3f4d30f80545047c4f076f26bd18ec4ae167
frederic.beaufort@gmail.com <frederic.beaufort@gmail.com@eacc61f9-9be6-2560-8530-41cdd8b5f1b6>
2013-04-13 20:08:51 +0000

    Locked landscape for the option and how to play pages

--- trunk/survivalkid/SurvivalKid/src/com/survivalkid/menu/OptionsActivity.java
+++ trunk/survivalkid/SurvivalKid/src/com/survivalkid/menu/OptionsActivity.java
@@ -1,6 +1,7 @@
 package com.survivalkid.menu;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
@@ -40,7 +41,7 @@ public class OptionsActivity extends AbstractActivity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
 				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		
         setContentView(R.layout.activity_options);
         
