e5993fc0c77fe77dd7c13b1897ab5cd211fa282c
Thomas Bonnin <thom159123@gmail.com>
2018-11-24 12:36:12 +0100

    Init SurvivalKid Project with libgdx and add all resources images

---
+++ survivalkidgdx/android/src/com/calliltbn/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.calliltbn;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.calliltbn.SurvivalKidGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SurvivalKidGame(), config);
+	}
+}
